1801b442847cd4909c808930a9faa164abd25002
mouche <144794503+Mouche00@users.noreply.github.com>
2023-11-22 15:17:15 +0000

    add client dashboard

---
+++ app/views/client/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="../../../public/assets/css/client/style.css">
+    <title>Client Dashboard</title>
+</head>
+<body class="flex h-[100vh]">
+    <section class="w-[17.5%] h-full bg-black text-white">
+        <img class="p-2 my-8" src="../../../public/assets/images/logo-white.png" alt="logo-black">
+        <div class="w-[70%] m-auto">
+            <img class="rounded-xl m-auto" src="../../../public/assets/images/profile.jpg" alt="profile">
+            <h2 class="m-auto">Morgan Freeman</h2>
+        </div>
+    </section>
+    <section class="w-[82.5%] h-full bg-white">
+
+    </section>
+</body>
+</html>
