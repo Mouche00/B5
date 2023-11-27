d12daf615ab990f5b08f44e6fdb3f9afb0f541d3
SoufianeNajim <110543784+soufianenajim99@users.noreply.github.com>
2023-11-27 10:15:39 +0100

    user table

--- app/models/user.php
+++ app/models/user.php
@@ -2,22 +2,39 @@
 
 require_once('DataProvider.php');
 
-class user extends DataProvider {
-   public function addUser($username,$nom, $prenom, $genre,$date_naissance,$nationalite,$password, $role) {
+class Users extends DataProvider {
+   public function addUser($username,$pw,$gendre,$role,$ville, $quartier,$rue,$codePostal,$email,$tel) {
     $db=$this->connect();
     if($db==null){
         return null;
    }
-   $sql= 'INSERT INTO user (username,nom,prenom,genre,date_naissance,nationalite,password) VALUES(:username,:nom,:prenom,:genre,:date_naissance,:nationalite,:password)';
+   $sql= 'START TRANSACTION;
+
+         INSERT INTO users (username, pw, gendre)
+         VALUES (:username,:pw,:gendre);
+   
+   
+         SET @userId = LAST_INSERT_ID();
+   
+        INSERT INTO roleofuser (userId, name)
+        VALUES (@userId, :role);
+   
+        INSERT INTO adress (ville, quartier,rue,codePostal,email,tel,userId)
+        VALUES ("test", "admin","test","test","test@gmail.com","00000",@userId);
+   
+        COMMIT;';
    $stmt = $db->prepare($sql);
    $stmt->execute([
     ':username'=> $username,
-    ":nom" => $nom,
-    ":prenom" => $prenom,
-    ":genre" => $genre,
-    ":date_naissance"=> $date_naissance,
-    ":nationalite"=> $nationalite,
-    ":password"=> $password
+    ":pw" => $pw,
+    ":gendre" => $gendre,
+    ":role" => $role,
+    ":ville"=> $ville,
+    ":quartier"=> $quartier,
+    ":rue"=> $rue,
+    ":codePostal"=> $codePostal,
+    ":email"=> $email,
+    ":tel"=> $tel
    ]);
    $db=null;
    $stmt=null;
