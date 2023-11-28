edcab6277129cea212e9249921f20fa5c09a0203
SoufianeNajim <110543784+soufianenajim99@users.noreply.github.com>
2023-11-28 10:35:53 +0100

    lastm

--- app/models/DataProvider.php
+++ app/models/DataProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 const CONFIG = [
-    'db'=>'mysql:dbname=db_bankmanagement;host=localhost;port:3306',
+    'db'=>'mysql:dbname=baank;host=localhost;port:3306',
     'db_user' => 'root',
     'db_password' => ''
 ];
